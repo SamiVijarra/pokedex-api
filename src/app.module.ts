51c2b10f54af4695f3559ac5764629e11aac3c6d
Sami <samivijarra1@gmail.com>
2026-05-18 23:57:53 -0300

    feat: add joi validation schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,14 @@ import { ConfigModule } from '@nestjs/config';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
-import { EnvConfiguration } from './config/app.config';
+import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema,
     }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
